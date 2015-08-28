b4e21cfab14fe041dcc15659d70c52d5c7ebf431
Davis Gao <naitnix@126.com>
2015-08-28 17:03:41 +0800

    remove some file

--- lib/movie_spider.rb
+++ lib/movie_spider.rb
@@ -1,11 +1,5 @@
 require "movie_spider/version"
-require 'movie_spider/douban'
-require 'movie_spider/tudou'
-require 'movie_spider/youku'
-require 'movie_spider/qq'
-require 'movie_spider/iqiyi'
 require 'movie_spider/baidu'
-require 'movie_spider/star'
 require 'movie_spider/tieba'
 require 'movie_spider/tball'
 require 'movie_spider/qqlive'
